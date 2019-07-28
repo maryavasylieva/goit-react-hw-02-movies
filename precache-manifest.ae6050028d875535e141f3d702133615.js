36c30b0bd86ad2124635d5c7e5ce09712f98fa25
maryavasylieva <miheta.laurelin@gmail.com>
2019-07-28 13:21:49 +0300

    Updates

---
+++ precache-manifest.ae6050028d875535e141f3d702133615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93f17680532430b964568f3e4ea64ee",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "3ecb9c61a2afc22ee1c7",
+    "url": "/goit-react-hw-02-movies/static/css/main.7072206c.chunk.css"
+  },
+  {
+    "revision": "2610701bb6da8c5ae02f",
+    "url": "/goit-react-hw-02-movies/static/js/2.692f68f2.chunk.js"
+  },
+  {
+    "revision": "3ecb9c61a2afc22ee1c7",
+    "url": "/goit-react-hw-02-movies/static/js/main.2d0db5c7.chunk.js"
+  },
+  {
+    "revision": "c0f0bf3555f23a72488d",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.4f93631a.js"
+  }
+]);
